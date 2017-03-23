58300f53be0d162ccd9b04ee0211367ef6ddbe91
Harp1e <github@bplayer.waitrose.com>
2017-03-23 09:39:12 +0000

    Initial ptoject setup - scenery and characters etc in place - lecture 47

---
+++ Assets/Enemies/Enemy.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+[RequireComponent(typeof(AICharacterControl))]
+public class Enemy: MonoBehaviour {
+
+    [SerializeField] float maxHealthPoints = 100f;
+    [SerializeField] float attackRadius = 4f;
+
+    AICharacterControl aiCharacterControl = null;
+    GameObject player = null;
+
+    float currentHealthPoints = 100f;
+
+    public float healthAsPercentage
+    {
+        get {
+            return currentHealthPoints / maxHealthPoints;
+        }
+    }
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        aiCharacterControl = GetComponent<AICharacterControl>();
+    }    private void Update()
+    {
+        float distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
+        if (distanceToPlayer <= attackRadius)
+        {
+            aiCharacterControl.SetTarget(player.transform);
+        }
+        else
+        {
+            aiCharacterControl.SetTarget(transform);
+        }
+    }
+
+}
---
+++ Assets/Enemies/EnemyUI.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+// Add a UI Socket transform to your enemy
+// Attach this script to the socket
+// Link to a canvas prefab that contains NPC UI
+public class EnemyUI : MonoBehaviour {
+
+    // Works around Unity 5.5's lack of nested prefabs
+    [Tooltip("The UI canvas prefab")]
+    [SerializeField] GameObject enemyCanvasPrefab = null;
+
+    Camera cameraToLookAt;
+
+    void Start()
+    {
+        cameraToLookAt = Camera.main;
+        Instantiate(enemyCanvasPrefab, transform.position, Quaternion.identity, transform);
+    }
+
+    void LateUpdate()
+    {
+        transform.LookAt(cameraToLookAt.transform);
+    }
+}
