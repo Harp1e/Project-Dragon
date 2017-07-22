94d6ea3c60feeabb0c34f25b850bc6ed17bdb8a5
Harp1e <github@bplayer.waitrose.com>
2017-07-22 19:50:48 +0100

    L137: From interface to inheritance

--- Assets/_Characters/Special Abilities/Self Heal/SelfHealBehaviour.cs
+++ Assets/_Characters/Special Abilities/Self Heal/SelfHealBehaviour.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 namespace RPG.Characters
 {
-    public class SelfHealBehaviour : MonoBehaviour, ISpecialAbility
+    public class SelfHealBehaviour : AbilityBehaviour
     {
         SelfHealConfig config = null;
         Player player = null;
@@ -16,7 +16,7 @@ namespace RPG.Characters
             this.config = configToSet;
         }
 
-        public void Use (AbilityUseParams useParams)
+        public override void Use (AbilityUseParams useParams)
         {
             ApplyHealth (useParams);
             PlayParticleEffect ();
