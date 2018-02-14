025f77b725b0d4233d61b9ee5d992b52215e6af4
Bob <github@bplayer.waitrose.com>
2018-02-14 16:02:54 +0000

    Enhance Minimap

---
+++ Assets/_Camera UI/MinimapFollow.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapFollow : MonoBehaviour 
+{
+    [SerializeField] Transform target;
+
+	void LateUpdate () 
+	{
+        if (!target) { return; }
+        Vector3 newPos = new Vector3 (target.position.x, transform.position.y, target.position.z);
+        transform.position = newPos;
+	}
+}
