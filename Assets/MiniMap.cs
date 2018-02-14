71cbc7a27c7eb2f4f808249821051cd5c0e0d8df
Bob <github@bplayer.waitrose.com>
2018-02-14 13:11:43 +0000

    Upgrade to 2017.3 and add basic minimap

---
+++ Assets/MiniMap.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMap : MonoBehaviour 
+{
+    [SerializeField] Transform target;
+    [SerializeField] float followSpeed = 5f;
+
+    float heightAbovePlayer = 30f;
+
+	void LateUpdate () 
+	{
+        if (!target) { return; }
+
+        Vector3 newPos = new Vector3 (
+            target.position.x,
+            target.position.y + heightAbovePlayer,
+            target.position.z
+        );
+        transform.position = newPos;
+	}
+}
